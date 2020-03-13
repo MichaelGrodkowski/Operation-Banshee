7586ec32c74c9292ddf9ec350b6c75acd3e061a2
Michael Grodkowski <michael_grodkowski@yahoo.com>
2020-03-12 20:05:24 -0600

    A whole lot of texturing.

---
+++ Level Design/Assets/Scripts/OnTrigger_door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrigger_door : MonoBehaviour
+{
+    private Animator doorAnim;
+    public GameObject toTrigger;
+    private void OnTriggerEnter(Collider toTrigger)
+    {
+        doorAnim.SetBool("Open", true);
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnim = this.transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Level Design/Assets/Scripts/hall_door.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hall_door : MonoBehaviour
+{
+
+    public Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("OpenDoor");   
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        anim.SetTrigger("CloseDoor");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        anim.SetTrigger("PauseOpen");
+    } 
+}
